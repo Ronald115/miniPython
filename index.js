f41e085e0d213efb8cc473d63393e3467a4f2df0
Ronald Arce <ronaldarce115@gmail.com>
2022-03-28 16:12:56 -0600

    pulgosas

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ const codeFolder = path.join(__dirname, 'code')
 
 app.use(express.urlencoded({extended:true}))
 
+page=path.join(app.get('public'),'/index.html')
+
 app.get("/", (req,res) =>{
-    res.sendFile(path.join(app.get('public'),'/index.html'))
+    res.sendFile(page)
 })
 
 app.post('/', (req,res) => {
@@ -24,20 +26,23 @@ app.post('/', (req,res) => {
     var fileContent = req.body.code
     fs.writeFile(filepath, fileContent, (err) => {
         if (err) throw err;
-    
         console.log("The file was succesfully saved!");
+        console.log(fileContent);
     }); 
+
+    res.sendFile(page);
+    
     // spawn new child process to call the python script
-    const python = spawn('python', ['main.py']);
+    //const python = spawn('python', ['main.py']);
     // collect data from script
-    python.stdout.on('data', (d) => {
-        data = d
-    });
+   // python.stdout.on('data', (d) => {
+    //    data = d
+    //});
         // in close event we are sure that stream from child process is closed
-        python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        res.send("post data received "+ data)
-    });
+ //      python.on('close', (code) => {
+  //     console.log(`child process close all stdio with code ${code}`);
+ //       res.send("post data received "+ data)
+ //   });
 
 
 })
